91336fcb26e676b683211aa66359f4386781cc40
kemplaw <kemplaw@hotmail.com>
2020-10-18 16:18:56 +0800

    [U] 更新文件内容

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent, onMounted, toRefs } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
   components: {
-    HelloWorld,
+    HelloWorld
   },
-});
+  props: {
+    title: {
+      type: String,
+      default: 'this is a title'
+    }
+  },
+  setup(prop) {
+    const { title } = toRefs(prop)
+
+    onMounted(() => console.log(title.value))
+
+    return {}
+  }
+})
 </script>
